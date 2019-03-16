9d4419c5e73adf06cc4b91f8d18a4b9e3ccf2b93
Aliaksandr Tarantsei <a.tarantey@gmail.com>
2019-03-16 17:57:03 +0300

    add remedly settings

--- Vagrantfile
+++ Vagrantfile
@@ -4,20 +4,20 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "bento/ubuntu-18.04"
 
-  config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.network "private_network", ip: "192.168.66.9"
 
   # config.vm.network "public_network"
 
-  config.vm.synced_folder "../vhosts", "/var/www/html", owner: "www-data", group: "www-data", mount_options: ['dmode=777', 'fmode=777']
+  config.vm.synced_folder "../vhosts/remedly/rm", "/home/vagrant/remedly/project", type: "nfs"
+  config.vm.synced_folder "../vhosts/remedly/wp", "/home/vagrant/remedly/wp", type: "nfs"
+  config.vm.synced_folder "../dumps", "/home/vagrant/dumps", type: "nfs"
+  config.vm.synced_folder "../vhosts/remedly/rm/data", "/home/vagrant/project/data", :mount_options => ["dmode=777","fmode=777"]
 
-  config.vm.provider :virtualbox do |vb|
-    vb.customize ["modifyvm", :id, "--memory", 4096, "--cpus", 2, "--ioapic", "on"]
+  config.vm.provider "virtualbox" do |vb|
+	vb.memory = "4096"
   end
 
-config.vm.provision :file, source: "vagrant/nginx/printmoment.conf", destination: "printmoment.conf"
-  config.vm.provision :file, source: "vagrant/nginx/volkovysk.conf", destination: "volkovysk.conf"
+  config.vm.provision :file, source: "vagrant/nginx/remedly.conf", destination: "remedly.conf"
   config.vm.provision :file, source: "vagrant/nginx/nginx.conf", destination: "nginx.conf"
-  config.vm.provision :file, source: "vagrant/mysql/my.cnf", destination: "my.cnf"
   config.vm.provision :shell, path: "vagrant/bootstrap.sh"
-  #config.vm.provision :shell, path: "vagrant/bootstrap_vagrant.sh", privileged: false
 end
--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -6,44 +6,47 @@
 apt-get update
 apt-get install -y curl git unzip
 
-# Install MySQL
-export DEBIAN_FRONTEND=noninteractive
-debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password password "
-debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password_again password "
-apt-get install -y mysql-server-5.7
-mv /home/vagrant/my.cnf /etc/my.cnf
-systemctl restart mysql
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';"
+# Install postgres
 
+apt-get install -y postgresql-10
+echo "-------------------- creating postgres vagrant role with password vagrant"
+
+#sudo su postgres -c "psql -c \"create user \"al_web_user\" with password 'web_pass'\""
+#sudo su postgres -c "psql -c \"create database aestheticlink\""
+
+#sudo su postgres -c "psql -c \"grant all privileges on database aestheticlink to \"al_web_user\"\""
+#sudo su postgres -c "psql -c \"grant all privileges ON all tables in schema public TO \"al_web_user\"\""
+
+#echo "-----------enable external connection"
+#echo "host all all 192.168.66.1/24 md5" >> /etc/postgresql/10/main/pg_hba.conf
+
+#echo "-----------fixing listen_addresses on postgresql.conf"
+#sudo sed -i "s/#listen_address.*/listen_addresses '*'/" /etc/postgresql/10/main/postgresql.conf
+  
+/etc/init.d/postgresql restart
 # Install php 7.2
 apt-get update
-apt-get install -y apache2 git curl php7.2 php7.2-fpm php7.2-mysqli php7.2-bcmath php7.2-bz2 php7.2-cli php7.2-curl php7.2-intl php7.2-json php7.2-mbstring php7.2-opcache php7.2-soap php7.2-sqlite3 php7.2-xml php7.2-xsl php7.2-zip php7.2-pgsql php7.2-xdebug libapache2-mod-php7.2
+apt-get install -y git curl poppler-utils imagemagick php7.2 php7.2-fpm php7.2-bcmath php7.2-bz2 php7.2-cli php7.2-curl php7.2-intl php7.2-json php7.2-mbstring php7.2-opcache php7.2-soap php7.2-sqlite3 php7.2-xml php7.2-xsl php7.2-zip php7.2-pgsql php7.2-xdebug libapache2-mod-php7.2
 
 mkdir -p /vagrant/www/html/tmp/profile
 
 xdebug= `cat <<EOS
-zend_extension=/usr/lib/php/20160303/xdebug.so
-html_errors=on
-xdebug.collect_vars=on
-xdebug.collect_params=4
-xdebug.dump_globals=on
-xdebug.dump.GET=*
-xdebug.dump.POST=*
-xdebug.show_local_vars=on
-xdebug.remote_enable = on
-xdebug.remote_autostart=on
-xdebug.remote_handler = dbgp
-xdebug.remote_connect_back=on
-xdebug.profiler_enable=0
-xdebug.profiler_output_dir="/vagrant/www/tmp/profile"
-xdebug.max_nesting_level=1000
-xdebug.remote_host=192.168.123.1
-xdebug.remote_port = 9001
-xdebug.idekey = "phpstorm"
+xdebug.remote_log=/var/log/xdebug.log
+xdebug.remote_handler=dbgp
+xdebug.remote_port=9123
+xdebug.collect_params=1
+xdebug.remote_connect_back=Off
+xdebug.remote_host=192.168.66.1
+xdebug.default_enable=1
+xdebug.profiler_enable_trigger=1
+xdebug.profiler_output_dir=/var/www
+xdebug.remote_autostart=1
+xdebug.idekey=PHPSTORM
+xdebug.show_local_vars=1                           
 EOS`
 
-echo $xdebug >> /etc/php/7.2/fpm/php.ini
-echo $xdebug >> /etc/php/7.2/cli/php.ini
+echo $xdebug > /etc/php/7.2/fpm/php.ini
+echo $xdebug > /etc/php/7.2/cli/php.ini
 
 cd /tmp
 curl -sS https://getcomposer.org/installer | php
@@ -55,8 +58,7 @@ systemctl disable apache2
 systemctl stop apache2
 apt-get install -y nginx
 mv /home/vagrant/nginx.conf /etc/nginx/nginx.conf
-mv /home/vagrant/volkovysk.conf /etc/nginx/sites-enabled/volkovysk.conf
-mv /home/vagrant/printmoment.conf /etc/nginx/sites-enabled/printmoment.conf
+mv /home/vagrant/remedly.conf /etc/nginx/sites-enabled/remedly.conf
 rm /etc/nginx/sites-enabled/default
 systemctl restart nginx
 systemctl enable nginx
